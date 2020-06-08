41a9b435453c7d4603c1c35e7fdbaa9efb2ad3a8
hosam-pc <hosamelshazly92@gmail.com>
2020-06-09 02:07:42 +0300

    add the reset of the form sections with styling

--- form.js
+++ form.js
@@ -8,36 +8,36 @@ const goButton = document.querySelector(".btn");
 
 function validateForm() {
     let name = nameInput.value;
-    
-    if(name == null || name == "") {
-        alert("a7a");
-        //stop the form from submitting
-        return false;
-    }
-    
-    if(name.length < 3) {
-        alert("a7eteen");
-        //stop the form from submitting
-        return false;
-    }
-}
 
-//goButton.onclick = function() {
-//    if (nameInput.value === "" || emailInput.value === "") {
-//        goButton.classList.add("error");
-//        msg.style.visibility = "visible";
-//
-//        setTimeout(function() {
-//            msg.style.visibility = "hidden";
-//            goButton.classList.remove("error");
-//        }, 2000);
-//    } else {
-//        const li = document.createElement("li");
-//        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
-//        userList.appendChild(li);
-//
-//        //clear fields
-//        nameInput.value = "";
-//        emailInput.value = "";
-//    }
-//}
+    //    if(name == null || name == "") {
+    //        alert("a7a");
+    //stop the form from submitting
+    //        return false;
+    //    }
+
+    //    if(name.length < 3) {
+    //        alert("a7eteen");
+    //stop the form from submitting
+    //        return false;
+    //    }
+    //}
+
+    //goButton.onclick = function() {
+    //    if (nameInput.value === "" || emailInput.value === "") {
+    //        goButton.classList.add("error");
+    //        msg.style.visibility = "visible";
+    //
+    //        setTimeout(function() {
+    //            msg.style.visibility = "hidden";
+    //            goButton.classList.remove("error");
+    //        }, 2000);
+    //    } else {
+    //        const li = document.createElement("li");
+    //        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+    //        userList.appendChild(li);
+    //
+    //        //clear fields
+    //        nameInput.value = "";
+    //        emailInput.value = "";
+    //    }
+    //}
